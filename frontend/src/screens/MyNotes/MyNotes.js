147ac522d6927064c807721d531d64a9e2c7e7ba
Soundarya-G <95346762+Soundarya-G@users.noreply.github.com>
2022-05-12 17:22:19 +0530

    updated mynotes, and added MainScreen.js and stylesheet commit

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,7 +1,85 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Accordion, Badge, Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import MainScreen from "../../components/MainScreen";
+import axios from "axios";
 
 const MyNotes = () => {
-  return <div>MyNotes</div>;
+  const [notes, setNotes] = useState([]);
+
+  const deleteHandler = () => {
+    if (window.confirm("Are you sure")) {
+    }
+  };
+
+  const fetchNotes = async () => {
+    const { data } = await axios.get("/api/notes");
+    setNotes(data);
+  };
+
+  console.log(notes);
+
+  useEffect(() => {
+    fetchNotes();
+  });
+
+  return (
+    <MainScreen title="Welcome Back Soundarya...">
+      <Link to="createnote">
+        <Button style={{ marginLeft: 10, marginButtom: 6 }} size="lg">
+          Create New Note
+        </Button>
+      </Link>
+      {notes.map((note) => (
+        <Accordion>
+          <Accordion.Item eventKey="0">
+            <Card style={{ margin: 10 }} key={note._id}>
+              <Accordion.Header as={Card.Text} variant="link" eventKey="0">
+                <Card.Header style={{ display: "flex", width: "100%" }}>
+                  <span
+                    style={{
+                      color: "white",
+                      textDecoration: "none",
+                      flex: 1,
+                      cursor: "pointer",
+                      alignSelf: "center",
+                      fontSize: 18,
+                    }}
+                  >
+                    {" "}
+                    {note.title}
+                  </span>
+                  <div>
+                    <Button href={`/note/${note._id}`}>Edit</Button>
+                    <Button
+                      variant="danger"
+                      className="mx-2"
+                      onClick={() => deleteHandler(note._id)}
+                    >
+                      Delete
+                    </Button>
+                  </div>
+                </Card.Header>
+              </Accordion.Header>
+              <Accordion.Body eventKey="0">
+                <Card.Body>
+                  <h4>
+                    <Badge variant="success">Category - {note.category}</Badge>
+                  </h4>
+                  <blockquote className="blockquote mb-0">
+                    <p>{note.content}</p>
+                    <footer className="blockquote-footer">
+                      Created On - date
+                    </footer>
+                  </blockquote>
+                </Card.Body>
+              </Accordion.Body>
+            </Card>
+          </Accordion.Item>
+        </Accordion>
+      ))}
+    </MainScreen>
+  );
 };
 
 export default MyNotes;
