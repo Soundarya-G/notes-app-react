59d719d74af8dc381cf2b2bd7e956a9de8a4aa03
Soundarya-G <95346762+Soundarya-G@users.noreply.github.com>
2022-05-12 15:50:32 +0530

    initialised frontend and backend

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  Container,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Form,
+  FormControl,
+} from "react-bootstrap";
+import mainLogo from "../../logo.png";
+
+const Header = () => {
+  return (
+    <Navbar bg="primary" expand="lg" variant="dark">
+      <Container>
+        <img src={mainLogo} className="img" alt="logo not visible" />
+        <Navbar.Brand href="/">Notes App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="m-auto">
+            <Form>
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="m-sm-2"
+              />
+            </Form>
+          </Nav>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">My Notes</Nav.Link>
+            <NavDropdown title="Soundarya" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">My profile</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
