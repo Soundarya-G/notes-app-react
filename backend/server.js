8edd811c6cd8bfae315feac42e1ec7006c839670
Soundarya-G <95346762+Soundarya-G@users.noreply.github.com>
2022-05-16 15:43:03 +0530

    connected Mongo DB and added Registration API

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,28 @@
-const express = require('express');
-const res = require('express/lib/response');
-const notes = require("./data/notes")
-const dotenv  = require('dotenv')
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/erroMiddlewares");
 
-const app =express();
+const app = express();
 dotenv.config();
+connectDB();
+app.use(express.json());
 
-
-app.get("/",(req, res) => {
-    res.send("API is running")
+app.get("/", (req, res) => {
+  res.send("API is running");
 });
 
 app.get("/api/notes", (req, res) => {
-    res.json(notes)
-})
+  res.json(notes);
+});
 
-app.get("/api/notes/:id", (req,res) => {
-    const note =notes.find((n) => n._id === req.params.id);
+app.use("/api/users", userRoutes);
 
-    res.json(note);
-})
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
-
