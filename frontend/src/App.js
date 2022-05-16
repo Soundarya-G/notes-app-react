3cee34ff5d40aa735c8dc1e3090ba4ef4ecbe4e9
Soundarya-G <95346762+Soundarya-G@users.noreply.github.com>
2022-05-16 18:37:09 +0530

    added frontend login authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,16 @@ import Header from "./components/Header/Header";
 import LandingPage from "./screens/LandingPage/LandingPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MyNotes from "./screens/MyNotes/MyNotes";
+import LoginScreen from "./screens/LoginScreen/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen/RegisterScreen";
 
 const App = () => (
   <BrowserRouter>
     <Header />
     <Routes>
       <Route path="/" element={<LandingPage />} exact />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
       <Route path="/mynotes" element={<MyNotes />} />
     </Routes>
     <Footer />
